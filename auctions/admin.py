892810435f78316f981d805906909efe5ba7b896
Ayush Kumar <shop.ayush930@gmail.com>
2020-07-22 16:34:40 +0530

    Added models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Listing, UserBid, UserComment, User
+
 # Register your models here.
+
+admin.site.register(Listing)
+admin.site.register(UserBid)
+admin.site.register(UserComment)
+admin.site.register(User)
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,34 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+#Create your models here.
+
+class Listing(models.Model):
+    title = models.CharField(max_length=130)
+    image = models.CharField(max_length=1000)
+    starting_bid = models.FloatField()
+    current_bid = models.FloatField()
+    bid_increment = models.FloatField()
+    condition = models.CharField(max_length=5)
+
+    def __str__(self):
+        return f"(ID: {self.id}) {self.title}"
+    
+
+class UserBid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    max_bid = models.FloatField()
+
+class UserComment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"{self.comment}"
+    
 
 class User(AbstractUser):
-    pass
+    watchlist = models.ManyToManyField(Listing, blank=True, related_name="watchers")
+    listing = models.ManyToManyField(Listing, blank=True, related_name="creater")
+    bids = models.ManyToManyField(UserBid, blank=True, related_name="bidder")
+    comments = models.ManyToManyField(UserComment, blank=True, related_name="commenter")
